58776360677b8ce2cd92ec08b7a0c3403532de07
Jan Brocher <jan.brocher@biovoxxel.de>
2022-01-27 11:16:57 +0100

    correct tile calculation for rest tiles at image border

--- clijx-dev/src/main/java/de/biovoxxel/clijx/plugins/PushGridTile.java
+++ clijx-dev/src/main/java/de/biovoxxel/clijx/plugins/PushGridTile.java
@@ -26,27 +26,7 @@ import net.haesleinhuepf.clij2.utilities.HasAuthor;
 @Plugin(type = CLIJMacroPlugin.class, name = "CLIJ2_pushGridTile")
 public class PushGridTile extends AbstractCLIJ2Plugin implements CLIJMacroPlugin, CLIJOpenCLProcessor, OffersDocumentation, HasAuthor {
 
-	//Test plugin
-	public static void main(String[] args) {
-	
-		IJ.run("Boats");
-		ImagePlus test_image = WindowManager.getCurrentImage();
-		CLIJ2 clij2 = CLIJ2.getInstance();
-
-		ClearCLBuffer test = clij2.push(test_image);
-		ImagePlus imp = clij2.pull(test);
-		imp.show();
-
-		// Test starts here
-		ClearCLBuffer tile = pushGridTile(clij2, imp, 4, 2, 1, 1, 1, 0, 40f);
-		System.out.println(tile);
-
-		clij2.print(tile);
-		ImagePlus tileImp = clij2.pull(tile);
-		tileImp.show();
-	}	
-	
-	
+		
 	public boolean executeCL() {
 		
 		String imageName = (String) args[0];
@@ -66,13 +46,34 @@ public class PushGridTile extends AbstractCLIJ2Plugin implements CLIJMacroPlugin
 		int tileZ = asInteger(args[6]);
 		float percentageOverlap = asFloat(args[7]);
 					
-		pushGridTile(getCLIJ2(), imp, tileCountX, tileCountY, tileCountZ, tileX, tileY, tileZ, percentageOverlap);
+		pushGridTile(getCLIJ2(), imp, imageName, tileCountX, tileCountY, tileCountZ, tileX, tileY, tileZ, percentageOverlap);
 				
 		return true;
 	}
 
+	/**
+	 * Method called via Macros
+	 */
+	public static void pushGridTile(CLIJ2 clij2, ImagePlus imp, String imageName, Integer tileCountX, Integer tileCountY, Integer tileCountZ, Integer tileX, Integer tileY, Integer tileZ, Float percentageOverlap) {
+		ClearCLBuffer buffer = pushGridTile(clij2, imp, tileCountX, tileCountY, tileCountZ, tileX, tileY, tileZ, percentageOverlap);
+		CLIJHandler.getInstance().pushInternal(buffer, imageName);
+	}
 	
+	/**
+	 * 
+	 * @param clij2 - CLIJ2 instance
+	 * @param imp - Source image
+	 * @param tileCountX - tile number in x-direction (1...n)
+	 * @param tileCountY - tile number in y-direction (1...n)
+	 * @param tileCountZ - tile-blocks number in z-direction (1...n)
+	 * @param tileX - tile identifier in x direction (0....n)
+	 * @param tileY - tile identifier in y direction (0....n)
+	 * @param tileZ - tile (stack-block) identifier in z direction (0....n)
+	 * @param percentageOverlap - tile overlap in percent (0 - 100)
+	 * @return ClearCLBuffer
+	 */
 	public static ClearCLBuffer pushGridTile(CLIJ2 clij2, ImagePlus imp, Integer tileCountX, Integer tileCountY, Integer tileCountZ, Integer tileX, Integer tileY, Integer tileZ, Float percentageOverlap) {
+		
 		
 		if (imp == null) {
 			imp = WindowManager.getCurrentImage();
@@ -80,7 +81,6 @@ public class PushGridTile extends AbstractCLIJ2Plugin implements CLIJMacroPlugin
 				throw new NullPointerException("The image you specified is not existing or there is no open image available.");
 			}
 		}
-		System.out.println("Working on image = " + imp);
 		
 		if (percentageOverlap < 0) {
 			percentageOverlap = 0.0f;
@@ -111,21 +111,39 @@ public class PushGridTile extends AbstractCLIJ2Plugin implements CLIJMacroPlugin
 		int tileHeight = getTileSize(tileCountY, tileY, nonOverlapFactor, imageHeight);
 		int tileDepth = getTileSize(tileCountZ, tileZ, nonOverlapFactor, imageDepth);
 		
-		int x_overlap = (int) Math.floor(tileWidth * overlapFactor);
-		int y_overlap = (int) Math.floor(tileHeight * overlapFactor);
-		int z_overlap = (int) Math.floor(tileDepth * overlapFactor);
+		int baseTileWidth = getTileSize(tileCountX, 0, nonOverlapFactor, imageWidth);
+		int baseTileHeight = getTileSize(tileCountY, 0, nonOverlapFactor, imageHeight);;
+		int baseTileDepth = getTileSize(tileCountZ, 0, nonOverlapFactor, imageDepth);
 		
-		System.out.println("x_overlap = " + x_overlap);
-		System.out.println("y_overlap = " + y_overlap);
-		System.out.println("z_overlap = " + z_overlap);
 		
-		ImageStack tileStack = imp.getImageStack().crop(tileX * tileWidth - tileX * x_overlap, tileY * tileHeight - tileY * y_overlap, tileZ * tileDepth - tileZ * z_overlap, tileWidth, tileHeight, tileDepth);
+		int x_overlap = (int) Math.ceil(baseTileWidth * overlapFactor);
+		int y_overlap = (int) Math.ceil(baseTileHeight * overlapFactor);
+		int z_overlap = (int) Math.ceil(baseTileDepth * overlapFactor);
+		
+		
+		int xLoc = tileX * baseTileWidth - tileX * x_overlap;
+		int yLoc = tileY * baseTileHeight - tileY * y_overlap;
+		int zLoc = tileZ * baseTileDepth - tileZ * z_overlap;
+		
+		
+		ImageStack tileStack = imp.getImageStack().crop(xLoc, yLoc, zLoc, tileWidth, tileHeight, tileDepth);
 		
 		ImagePlus tileImagePlus = new ImagePlus("current_tile", tileStack);
 		
-		System.out.println("Pushing " + tileImagePlus + " to GPU");
 		ClearCLBuffer buffer = clij2.push(tileImagePlus);
 
+		System.out.println("---> Processing tile = " + tileX + " / " + tileY + " / " + tileZ);
+		System.out.println("Working on image = " + imp);
+		System.out.println("tileWidth * overlapFactor = " + baseTileWidth * overlapFactor);
+		System.out.println("x_overlap = " + x_overlap);
+		System.out.println("tileHeight * overlapFactor = " + baseTileHeight * overlapFactor);
+		System.out.println("y_overlap = " + y_overlap);
+		System.out.println("tileDepth * overlapFactor = " + baseTileDepth * overlapFactor);
+		System.out.println("z_overlap = " + z_overlap);
+		System.out.println("xLoc = " + xLoc);
+		System.out.println("yLoc = " + yLoc);
+		System.out.println("zLoc = " + zLoc);
+		System.out.println("Pushing " + tileImagePlus + " to GPU");
 		System.out.println(buffer);
 		
 		return buffer;
@@ -135,19 +153,13 @@ public class PushGridTile extends AbstractCLIJ2Plugin implements CLIJMacroPlugin
 	private static int getTileSize(Integer gridTileCount, Integer tilePositionID, float nonOverlapFactor, int imageSize) {
 		int tileSize = (int) Math.floor(imageSize / (1 + (gridTileCount - 1) * nonOverlapFactor));
 		//in case the current tile is the last one in this row / column / slice-block use the rest of the remaining image 
-		if (tilePositionID == gridTileCount) {
-			tileSize = imageSize - ((tilePositionID - 1) * tileSize);
+		if (tilePositionID == gridTileCount-1) {
+			tileSize = (int) Math.floor(imageSize - (Math.floor(tileSize * nonOverlapFactor) * (gridTileCount - 1)));
 		}
-		System.out.println("tileWidth = " + tileSize);
+		System.out.println("tileSize = " + tileSize);
 		return tileSize;
 	}
 	
-	
-	//Not sure if that method is necessary at all???
-	public static void pushGridTile(CLIJ2 clij2, ImagePlus imp, String imageName, Integer tileCountX, Integer tileCountY, Integer tileCountZ, Integer tileX, Integer tileY, Integer tileZ, Float percentageOverlap) {
-		ClearCLBuffer buffer = pushGridTile(clij2, imp, tileCountX, tileCountY, tileCountZ, tileX, tileY, tileZ, percentageOverlap);
-		CLIJHandler.getInstance().pushInternal(buffer, imageName);
-	}
 	
 	
 	@Override
@@ -204,6 +216,25 @@ public class PushGridTile extends AbstractCLIJ2Plugin implements CLIJMacroPlugin
 		return "Jan Brocher";
 	}
 
+	//Test plugin
+	public static void main(String[] args) {
+	
+		IJ.run("Boats");
+		ImagePlus test_image = WindowManager.getCurrentImage();
+		CLIJ2 clij2 = CLIJ2.getInstance();
+
+		ClearCLBuffer test = clij2.push(test_image);
+		ImagePlus imp = clij2.pull(test);
+		imp.show();
+
+		// Test starts here
+		ClearCLBuffer tile = pushGridTile(clij2, imp, 4, 2, 1, 1, 1, 0, 40f);
+		System.out.println(tile);
+
+		clij2.print(tile);
+		ImagePlus tileImp = clij2.pull(tile);
+		tileImp.show();
+	}	
 
 	
 }
